3a465effad647b5f75e359c707471f8ac274f66f
sitakant <chaudhury.sitakant@gmail.com>
2018-11-12 12:26:53 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HelloModule} from '@sitakant/say-hello/src/app/app.module'
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from '@sitakant/say-hello/src/app/app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule
